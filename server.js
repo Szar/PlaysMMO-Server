3993e745c262d2c905411db9de62ca165f1428a8
Ubuntu <ubuntu@ip-172-31-41-217.ec2.internal>
2019-05-14 23:10:10 +0000

    Added twitch chat integration

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const express = require('express'),
 		key:  fs.readFileSync(config.server.ssl_dir+'privkey.pem'),
 		cert: fs.readFileSync(config.server.ssl_dir+'cert.pem'),
 		secureProtocol: 'SSLv23_method',
-	};
+	},
+	TwitchJS = require('twitch-js')
 
 var io = require('socket.io').listen(require('https').createServer(credentials, app).listen(8000)),
-	i = 0;
+	i = 0,
+	chat_clients = {},
+	channels = [],
+	ids = {}
 
 var getUser = function(data, c){
 	fetch("https://api.twitch.tv/helix/users?id="+data["user_id"], { 
@@ -86,8 +90,33 @@ io.on('connection', function(socket){
 		socket.player.name = data["user_id"]
 		getUser(data, function(d){
 			socket.player.name = d["display_name"]
+			ids[socket.player.name] = socket.player.id
 			io.sockets.in(socket.channel).emit('update', socket.player);
-			
+			if(channels.indexOf(socket.channel) === -1){
+				channels.push(socket.channel);
+				getUser({ 
+					"user_id":data["channelId"],
+					"clientId":"9zc9rmh14ylh4b1y3z9a0tavpe0qym" }, function(d){
+					var channel_name = "#"+d["login"]
+					chat_clients[channel_name] = new TwitchJS.client({
+						channels: [channel_name], 
+						identity: {
+						username: "FukleBot",
+						password: "oauth:93girx4jk9z6fm41goffk8flpdcnso"
+						},
+					});
+					chat_clients[channel_name].on('chat', (channel, userstate, message, self) => {
+	
+						io.sockets.in(socket.channel).emit('message',{
+							"id":ids[userstate['display-name']],
+							"message": message
+						});
+						//console.log(`Message "${message}" received from ${userstate['display-name']}`);
+						if (self) return;
+					});
+					chat_clients[channel_name].connect();
+				});	
+			}
 		});	
 	});
 	socket.on('disconnect',function(){
